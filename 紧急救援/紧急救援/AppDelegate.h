d9141efea907f8ccb03973294f120b472c95b845
lcy <454036111@qq.com>
2015-07-29 16:48:28 +0800

    框架
    测试

---
+++ 紧急救援/紧急救援/AppDelegate.h
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.h
+//  紧急救援
+//
+//  Created by 刘成勇 on 15/7/29.
+//  Copyright (c) 2015年 紧急救援. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <CoreData/CoreData.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
+
+@end
+
